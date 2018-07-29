ec6a3faf820c7f3af90884d147b253e56cc8db0c
grapeperson <grapeperson@inbox.ru>
2018-07-29 21:24:27 +0300

    fixed_config_path

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: './'
   },
   devServer: {
+    publicPath: '/',
     historyApiFallback: true,
   },
   performance: {
